b3171ae42bb5e3352c7c32725ace2017e010b901
T@rik <127018435+Ttoru140@users.noreply.github.com>
2025-05-22 10:45:06 +0600

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #define RCC_BASE 0x40021000
-#define RCC_APB2ENR (*(volatile unsigned int *)(RCC_BASE + 0x18))
-#define RCC_APB1ENR (*(volatile unsigned int *)(RCC_BASE + 0x1C))
+#define RCC_APB2ENR (*(volatile unsigned int *)(RCC_BASE + 0x18))  //Controls clock for GPIO
+#define RCC_APB1ENR (*(volatile unsigned int *)(RCC_BASE + 0x1C))  //Controls clock for Timer2.
 
 #define GPIOC_BASE 0x40011000
 #define GPIOC_CRH  (*(volatile unsigned int *)(GPIOC_BASE + 0x04))
@@ -15,16 +15,16 @@
 
 int main(){
 
-	RCC_APB2ENR |=(1u << 4);
-	RCC_APB1ENR |=(1u << 0);
+	RCC_APB2ENR |=(1u << 4);    // Enable clock for GPIOC
+	RCC_APB1ENR |=(1u << 0);     // Enable clock for TIM2
 
 
-	GPIOC_CRH &=~(0xF << 20);
-	GPIOC_CRH |=(1u << 21);
+	GPIOC_CRH &=~(0xF << 20);   // Clear CNF13[1:0] and MODE13[1:0]
+	GPIOC_CRH |=(1u << 21);    // Set MODE13 = 0b01 (Output mode, max speed 10 MHz)
 
-	TIM2_PSC = 7999;
-	TIM2_ARR = 999;
-	TIM2_CR1 |= (1u << 0);
+	TIM2_PSC = 7999;        // Prescaler: divide clock by 8000 (assuming 8 MHz -> 1 kHz)      Divides the system clock down to slow the timer.
+	TIM2_ARR = 999;          // Auto-reload value: 999 + 1 = 1000 (1 Hz)     Sets the value at which the timer overflows (resets to 0 and sets the update flag UIF).
+	TIM2_CR1 |= (1u << 0);    // Enable counter
 
 
 
@@ -32,9 +32,9 @@ int main(){
 //		GPIOC_ODR ^=(1u << 13);
 //		for(volatile int i =0; i<100000;i++);
 
-		if(TIM2_SR &(1u << 0)){
-			TIM2_SR &=~(1u<<0);
-			GPIOC_ODR ^=(1u << 13);
+		if(TIM2_SR &(1u << 0)){       // If UIF (update interrupt flag) is set
+			TIM2_SR &=~(1u<<0);         // Clear the UIF flag
+			GPIOC_ODR ^=(1u << 13);            // Toggle PC13
 		}
 
 	}
